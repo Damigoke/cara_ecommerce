303320d0469e4be26eb94e147fe1a3869cbb35e4
Mofopefoluwa <fopefaokunla@gmail.com>
2024-04-11 18:45:31 +0100

    commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,29 +1,31 @@
 import { Sequelize } from "sequelize";
-import { DB_URL }  from "./config";
-
+import { DB_URL } from "./config";
 
 function dbconnection() {
-    const sslOptions = {
-        rejectUnauthorized: false  // Set rejectUnauthorized to true
-    };
+  const sslOptions = {
+    rejectUnauthorized: false, // Set rejectUnauthorized to true
+  };
 
-    const sequelize = new Sequelize(DB_URL, {
-        dialect: "mysql",
-        dialectOptions: {
-            ssl: sslOptions
-        },
-        // timezone: '+00:00',
+  const sequelize = new Sequelize(DB_URL, {
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: sslOptions,
+    },
+    // timezone: '+00:00',
+  });
+
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log(
+        "Connection to the database has been established successfully."
+      );
+    })
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
     });
 
-    sequelize.authenticate()
-        .then(() => {
-            console.log('Connection to the database has been established successfully.');
-        })
-        .catch(err => {
-            console.error('Unable to connect to the database:', err);
-        });
-
-    return sequelize;
+  return sequelize;
 }
 
 export default dbconnection;
