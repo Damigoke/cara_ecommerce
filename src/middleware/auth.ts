2b4f0258d9499efd6060e4cee6e1a26a9d3e9732
Damilola <d.botanist20@gmail.com>
2024-05-31 07:20:31 +0100

    fix_auth_issues

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,6 +5,40 @@ import { jwtsecret } from '../config/config';
 import path from "path";
 
 
+export async function adminAuth(req: Request | any, res: Response, next: NextFunction) {
+    try {
+        const authorization = req.headers.authorization;
+
+        if (!authorization) {
+            return res.status(401).json({ error: 'Kindly Log in as a User' })
+        }
+
+        const token = authorization?.slice(7, authorization.length);
+
+        let verified = jwt.verify(token, jwtsecret);
+
+        if (!verified) {
+            return res.status(401).send({ error: "Token not available on this route" });
+        }
+
+        const { id, role } = verified as { id: string, role: string }; // Assuming the role is included in the JWT payload
+
+        // Find user by id
+        const user = await User.findOne({ where: { id } });
+
+        if (user && role !== 'admin') {
+             return res.status(401).json({ Error: "Kindly login correct details as a user" });
+        }
+
+        req.user = verified;
+        next();
+
+    } catch (err) {
+        return res.status(400).send("User not Logged in");
+    }
+}
+
+
 export async function auth(req: Request | any, res: Response, next: NextFunction) {
     try {
         const authorization = req.headers.authorization;
@@ -37,7 +71,6 @@ export async function auth(req: Request | any, res: Response, next: NextFunction
         return res.status(400).send("User not Logged in");
     }
 }
-
 export function getContentType(filePath: string) {
   const ext = path.extname(filePath).toLowerCase();
   switch (ext) {
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -2,16 +2,16 @@ import express, { Request, Response, NextFunction } from 'express';
 //import { deleteProduct, CreateProduct, UpdateProduct, uploadProductImages} from '../controller/productController';
 const router = express.Router();
 import { createproduct, allWooCommerceProducts, updateproduct, deleteproduct, getSingleProducts, createcategoryEndpoint, getallCategories, deleteCategoryEndpoint } from '../controller/productController';
-import { auth } from '../middleware/auth';
+import { adminAuth } from '../middleware/auth';
 
 /* GET home page. */
-router.post('/createproduct', createproduct)
-router.post('/createcategory', createcategoryEndpoint)
+router.post('/createproduct', adminAuth, createproduct)
+router.post('/createcategory', adminAuth, createcategoryEndpoint)
 router.get('/getAllproduct', allWooCommerceProducts)
-router.get('/getAllCategory', getallCategories)
-router.get('/getproduct/:productId', getSingleProducts)
-router.get('/update-product/:productId', updateproduct)
-router.get('/delete-product/:productId', deleteproduct)
-router.delete('/delete-category/:id', deleteCategoryEndpoint)
+router.get('/getAllCategory', adminAuth, getallCategories)
+router.get('/getproduct/:productId',  getSingleProducts)
+router.get('/update-product/:productId', adminAuth, updateproduct)
+router.get('/delete-product/:productId', adminAuth, deleteproduct)
+router.delete('/delete-category/:id', adminAuth, deleteCategoryEndpoint)
 
 export default router;
